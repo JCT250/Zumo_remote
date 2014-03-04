ba4020c4ade2121839096d271a20d85fbfb3db99
JCT250 <james.taylor250@gmail.com>
2014-03-05 10:17:08 +1300

    Added OT version
    Load the OT remote software onto the Arduino, then create a bluetooth
    serial connection between a computer and the robot. Running the PC side
    software will allow you to start and stop the robot driving around and
    playing a song with the press of a button

---
+++ OT Software/OT Software/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_serial_connect = new System.Windows.Forms.Button();
+            this.btn_serial_disconnect = new System.Windows.Forms.Button();
+            this.btn_click = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.lbl_serial = new System.Windows.Forms.Label();
+            this.btn_serial_refresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_serial_connect
+            // 
+            this.btn_serial_connect.Location = new System.Drawing.Point(12, 39);
+            this.btn_serial_connect.Name = "btn_serial_connect";
+            this.btn_serial_connect.Size = new System.Drawing.Size(127, 23);
+            this.btn_serial_connect.TabIndex = 2;
+            this.btn_serial_connect.Text = "Connect";
+            this.btn_serial_connect.UseVisualStyleBackColor = true;
+            this.btn_serial_connect.Click += new System.EventHandler(this.btn_serial_connect_Click);
+            // 
+            // btn_serial_disconnect
+            // 
+            this.btn_serial_disconnect.Location = new System.Drawing.Point(145, 39);
+            this.btn_serial_disconnect.Name = "btn_serial_disconnect";
+            this.btn_serial_disconnect.Size = new System.Drawing.Size(127, 23);
+            this.btn_serial_disconnect.TabIndex = 3;
+            this.btn_serial_disconnect.Text = "Disconnect";
+            this.btn_serial_disconnect.UseVisualStyleBackColor = true;
+            this.btn_serial_disconnect.Click += new System.EventHandler(this.btn_serial_disconnect_Click);
+            // 
+            // btn_click
+            // 
+            this.btn_click.BackColor = System.Drawing.Color.Yellow;
+            this.btn_click.Location = new System.Drawing.Point(16, 98);
+            this.btn_click.Name = "btn_click";
+            this.btn_click.Size = new System.Drawing.Size(256, 152);
+            this.btn_click.TabIndex = 6;
+            this.btn_click.Text = "Click";
+            this.btn_click.UseVisualStyleBackColor = false;
+            this.btn_click.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btn_click_KeyDown);
+            this.btn_click.KeyUp += new System.Windows.Forms.KeyEventHandler(this.btn_click_KeyUp);
+            this.btn_click.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btn_click_MouseDown);
+            this.btn_click.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn_click_MouseUp);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(260, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // lbl_serial
+            // 
+            this.lbl_serial.Location = new System.Drawing.Point(13, 69);
+            this.lbl_serial.Name = "lbl_serial";
+            this.lbl_serial.Size = new System.Drawing.Size(127, 23);
+            this.lbl_serial.TabIndex = 5;
+            this.lbl_serial.Text = "Serial Connection Closed";
+            this.lbl_serial.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_serial_refresh
+            // 
+            this.btn_serial_refresh.Location = new System.Drawing.Point(146, 69);
+            this.btn_serial_refresh.Name = "btn_serial_refresh";
+            this.btn_serial_refresh.Size = new System.Drawing.Size(127, 23);
+            this.btn_serial_refresh.TabIndex = 4;
+            this.btn_serial_refresh.Text = "Refresh";
+            this.btn_serial_refresh.UseVisualStyleBackColor = true;
+            this.btn_serial_refresh.Click += new System.EventHandler(this.btn_serial_refresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btn_serial_refresh);
+            this.Controls.Add(this.lbl_serial);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btn_click);
+            this.Controls.Add(this.btn_serial_disconnect);
+            this.Controls.Add(this.btn_serial_connect);
+            this.Name = "Form1";
+            this.Text = "ZumOT";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_serial_connect;
+        private System.Windows.Forms.Button btn_serial_disconnect;
+        private System.Windows.Forms.Button btn_click;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label lbl_serial;
+        private System.Windows.Forms.Button btn_serial_refresh;
+    }
+}
+
---
+++ OT Software/OT Software/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var ports = SerialPort.GetPortNames();
+            comboBox1.DataSource = ports;
+            this.KeyPreview = true;
+        }
+
+        
+
+        private void Connect(string portName)
+        {
+            serialPort1 = new SerialPort(portName);
+            if (!serialPort1.IsOpen)
+            {
+                serialPort1.BaudRate = 9600;
+                serialPort1.Open();
+                btn_serial_connect.Enabled = false;
+                lbl_serial.Text = (String.Format("Connected to '{0}'", comboBox1.SelectedItem));
+            }
+        } //done  
+
+        private void serial_check()
+        {
+            MessageBox.Show("Please open Serial Connection");
+        } //done
+
+        private void btn_serial_connect_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex > -1)
+            {
+                // MessageBox.Show(String.Format("Connecting to '{0}'", comboBox1.SelectedItem));
+                Connect(comboBox1.SelectedItem.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Please select a port first");
+            }
+        } //done
+
+        private void btn_serial_disconnect_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                lbl_serial.Text = ("Serial Connection Closed");
+                // MessageBox.Show("Serial connection closed");
+                btn_serial_connect.Enabled = true;
+            }
+            else if (!serialPort1.IsOpen)
+            {
+                MessageBox.Show("No connection currently open", "Alert");
+            }
+        } //done
+
+        private void btn_serial_refresh_Click(object sender, EventArgs e)
+        {
+            var ports = SerialPort.GetPortNames();
+            comboBox1.DataSource = ports;
+        }
+
+        private void btn_click_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                serial_check();
+            }
+            else
+            {
+                btn_click.BackColor = Color.Green;
+                Byte[] down = {0x44};
+                serialPort1.Write(down, 0, 1);
+            }
+        }
+
+        private void btn_click_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                serial_check();
+            }
+            else
+            {
+                btn_click.BackColor = Color.Yellow;
+                Byte[] up = { 0x55 };
+                serialPort1.Write(up, 0, 1);
+            }
+        }
+
+        private void btn_click_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                serial_check();
+            }
+            else
+            {
+                btn_click.BackColor = Color.Green;
+                Byte[] down = { 0x44 };
+                serialPort1.Write(down, 0, 1);
+            }
+        }
+
+        private void btn_click_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                serial_check();
+            }
+            else
+            {
+                btn_click.BackColor = Color.Yellow;
+                Byte[] up = { 0x55 };
+                serialPort1.Write(up, 0, 1);
+            }
+        }
+    }
+}
